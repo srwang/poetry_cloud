05652414b5ccc2152db0176d2514250b0072170e
Sharon Wag <sharon.ran.wang@gmail.com>
2016-03-25 15:32:36 -0400

    angular express file structure added

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-var request = require("request"),
-	cheerio = require("cheerio"),
-	url = "http://www.poetryfoundation.org/poetrymagazine/poem/" + 252078;
-  
-request(url, function (error, response, body) {
-	if (!error) {
-		var $ = cheerio.load(body),
-			title = $('#poem-top > h1').text(),
-			author = $('#poemwrapper > span.author > a').text(),
-			bodyHtml = $('#poem > div.poem').html();
-		  
-		console.log(title);
-		console.log(author);  
-		console.log(bodyHtml);
-	} else {
-		console.log("We’ve encountered an error: " + error);
-	}
+
+/**
+ * Module dependencies
+ */
+
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  morgan = require('morgan'),
+  routes = require('./routes'),
+  api = require('./routes/api'),
+  http = require('http'),
+  path = require('path');
+
+var app = module.exports = express();
+
+
+/**
+ * Configuration
+ */
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/**
+ * Routes
+ */
+
+// serve index and view partials
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+
+// JSON API
+app.get('/api/name', api.name);
+
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+
+/**
+ * Start Server
+ */
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
 });
